74910dd702b2ce54b1c4c66f80d6c30909cbd931
Matheus Merp <94021579+MERP13@users.noreply.github.com>
2023-08-20 20:04:00 -0300

    s

---
+++ scripts/modal.js
@@ -0,0 +1,18 @@
+// script.js
+const openModalBtn = document.getElementById("openModalBtn");
+const modal = document.getElementById("myModal");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+openModalBtn.addEventListener("click", function() {
+    modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
